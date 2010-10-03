44fa3a503fffeeb681f62c3a1ea147f7131a5817
atom smith <re5etsmyth@gmail.com>
2010-10-02 20:04:10 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,26 @@
+Github Repo Widgets
+===========
+
+Github Repo Widgets is a MooTools plugin that allows you to fetch information regarding a Github repository using a JSONP request, and builds an HTML widget for you.
+
+![Screenshot](http://re5et.github.com/projects/github-repo-widget/assets/images/github-repo-widget-screenshot.png)
+
+How to use
+----------
+
+You can use GithubRepoWidget to create a widget by doing the following:
+
+    var myRepoWidget = new GithubRepoWidget('mootools', 'mootools-more');
+    myRepoWidget.addEvent('complete', function(response, widget){
+        $('myElement').grab(widget);
+    });
+
+or you can supply an element to inject into and have the complete event handled for you:
+
+   var myRepoWidget = new GithubRepoWidget('re5et', '.dotfiles', {
+       'injectInto': $('myElement')
+   });
+
+You can also use hoverCards, which will automatically find github repo links and attach hovercards to them, by running:
+
+   GithubRepoWidget.hovercards()
---
+++ Source/github-repo-widget.hovercards.js
@@ -0,0 +1,43 @@
+/*
+---
+description: Github repo widget hovercards.  Automatically finds github repo links and applies a github repo widget hovercard to the link
+
+license: MIT-style
+
+authors: [atom smith]
+
+requires:
+- GithubRepoWidget
+- more/1.2.4.4: [Element.Position]
+
+provides: [GithubRepoWidget.hoverCards]
+---
+*/
+
+GithubRepoWidget.hoverCards = function(){
+    var cards = [];
+    $$('a[href^="http://github.com"]').each(function(a){
+	var chunks = a.get('href').split('/');
+	var user = chunks[3];
+	var repo = chunks[4];
+	if(user && repo)
+	{
+	    var card = new GithubRepoWidget(user, repo).addEvent('complete', function(response, container){
+		container.setStyle('display','none');
+		$(document.body).grab(container);
+		container.addEvent('mouseleave', function(){
+		    container.setStyle('display', 'none');
+		})
+		a.addEvent('mouseenter', function(){
+		    container.position({
+	    		'relativeTo': a,
+	    		'position': 'upperleft'
+		    });
+	    	    container.setStyle('display', 'block');
+		});
+	    });
+	}
+	cards.push(card);
+    });
+    return cards;
+};
