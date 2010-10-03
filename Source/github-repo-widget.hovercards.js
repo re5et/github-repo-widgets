a4d176445f85aaddf183998ac4a6a3d9bb4e7be8
atom smith <re5etsmyth@gmail.com>
2010-10-03 01:41:56 -0400

    fixed whitespace, added ability to override options in the position setting

--- Source/github-repo-widget.hovercards.js
+++ Source/github-repo-widget.hovercards.js
@@ -2,7 +2,7 @@
 ---
 script: github-repo-widget.hovercards.js
 
-description: Github repo widget hovercards.  Automatically finds github repo links and applies a github repo widget hovercard to the link
+description: Github repo widget hovercards.	 Automatically finds github repo links and applies a github repo widget hovercard to the link
 
 license: MIT-style
 
@@ -16,34 +16,31 @@ provides: [GithubRepoWidget.hoverCards]
 ...
 */
 
-GithubRepoWidget.hoverCards = function(position){
-    position = position || {
-	'x': 'left',
-	'y': 'bottom'
-    };
-    var cards = [];
-    $$('a[href^="http://github.com"]').each(function(a){
-	var chunks = a.get('href').split('/');
-	var user = chunks[3];
-	var repo = chunks[4];
-	if(user && repo)
-	{
-	    var card = new GithubRepoWidget(user, repo).addEvent('complete', function(response, container){
-		container.setStyle('display','none');
-		$(document.body).grab(container);
-		container.addEvent('mouseleave', function(){
-		    container.setStyle('display', 'none');
-		})
-		a.addEvent('mouseenter', function(){
-		    container.position({
-	    		'relativeTo': a,
-	    		'position': position
-		    });
-	    	    container.setStyle('display', 'block');
-		});
-	    });
-	}
-	cards.push(card);
-    });
-    return cards;
+GithubRepoWidget.hoverCards = function(options){
+	options = options || {}
+	var cards = [];
+	$$('a[href^="http://github.com"]').each(function(a){
+		var chunks = a.get('href').split('/');
+		var user = chunks[3];
+		var repo = chunks[4];
+		if(user && repo)
+		{
+			var card = new GithubRepoWidget(user, repo).addEvent('complete', function(response, container){
+				container.setStyle('display','none');
+				$(document.body).grab(container);
+				container.addEvent('mouseleave', function(){
+					container.setStyle('display', 'none');
+				})
+				a.addEvent('mouseenter', function(){
+					container.position($merge({
+						'relativeTo': a,
+						'position': 'bottomLeft'
+					}, options));
+					container.setStyle('display', 'block');
+				});
+			});
+		}
+		cards.push(card);
+	});
+	return cards;
 };
